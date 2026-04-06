73bfd719e4c8e99518d8152ba39560b2dcc4c5d0
fardinabir <fardinabir@gmail.com>
2026-04-07 06:46:08 +0900

    chore: remove old version of project

--- rules-resolution-service-old/migrations/001_create_steps.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE steps (
-    key         TEXT PRIMARY KEY,
-    name        TEXT NOT NULL,
-    description TEXT,
-    position    INT  NOT NULL
-);
--- rules-resolution-service-old/migrations/002_create_defaults.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE defaults (
-    step_key    TEXT NOT NULL REFERENCES steps(key),
-    trait_key   TEXT NOT NULL,
-    value       JSONB NOT NULL,
-    PRIMARY KEY (step_key, trait_key)
-);
