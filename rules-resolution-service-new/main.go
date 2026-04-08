a2015574daf1ad7a570b67c038ac17a8bc00f9e3
fardinabir <fardinabir@gmail.com>
2026-04-09 03:12:53 +0900

    chore: ignore compiled Go binaries

--- rules-resolution-service-new/main.go
+++ rules-resolution-service-new/main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"github.com/fardinabir/go-svc-boilerplate/cmd"
-	_ "github.com/fardinabir/go-svc-boilerplate/docs"
-)
+import "github.com/fardinabir/go-svc-boilerplate/cmd"
 
-// @title         Users Service API
-// @version       v1.0
-// @description   User CRUD API for user-management service.
-// @license.name  MIT
-// @license.url   https://opensource.org/licenses/MIT
-// @host          localhost:8082
-// @BasePath      /api/v1
-// @schemes       http
 func main() {
 	cmd.Execute()
 }
