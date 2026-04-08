cfe5562f81a7a50ce3fdefc98f4c437a1b41c4fb
fardinabir <fardinabir@gmail.com>
2026-04-09 04:05:26 +0900

    chore: update postgres port config and swagger comment in main.go

--- rules-resolution-service-new/main.go
+++ rules-resolution-service-new/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/fardinabir/go-svc-boilerplate/cmd"
+import (
+	"github.com/fardinabir/go-svc-boilerplate/cmd"
+	_ "github.com/fardinabir/go-svc-boilerplate/docs"
+)
 
+// @title         Users Service API
+// @version       v1.0
+// @description   User CRUD API for user-management service.
+// @license.name  MIT
+// @license.url   https://opensource.org/licenses/MIT
+// @host          localhost:8082
+// @BasePath      /api/v1
+// @schemes       http
 func main() {
 	cmd.Execute()
 }
